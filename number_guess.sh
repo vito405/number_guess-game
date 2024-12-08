2d6a6f40e6f9154216ae32a172cae3800382e197
vito 405 <vitomortalo99@gmail.com>
2024-12-08 21:19:01 +0000

    feat: add games counter

--- number_guess.sh
+++ number_guess.sh
@@ -7,18 +7,20 @@ read USER
 
 SECRET_NUMBER=$(( (RANDOM % 1000) + 1 ))
 echo $SECRET_NUMBER
+
 # Check if the user exists
 FIND=$($PSQL "SELECT name FROM users WHERE name='$USER'")
 
 if [[ -z $FIND ]]; then
   echo "Welcome, $USER! It looks like this is your first time here."
 
-  ADD=$($PSQL "INSERT INTO users(name) VALUES('$USER')")
+  ADD=$($PSQL "INSERT INTO users(name, games_played) VALUES('$USER', 0)")
 
   # Prompt for the first guess
   echo "Guess the secret number between 1 and 1000:"
 else
-  echo "Welcome back, $USER! You have played <games_played> games, and your best game took <best_game> guesses."
+  GAMES=$($PSQL "SELECT games_played FROM users WHERE name='$USER'")
+  echo "Welcome back, $USER! You have played $GAMES games, and your best game took <best_game> guesses."
   echo "Guess the secret number between 1 and 1000:"
 fi
 
@@ -26,6 +28,12 @@ fi
 NUMBER_OF_GUESSES=0
 while true; do
   read GUESS
+
+  # Validate that input is an integer
+  if ! [[ $GUESS =~ ^[0-9]+$ ]]; then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
 
   # Increment the guess counter
   ((NUMBER_OF_GUESSES++))
@@ -37,6 +45,7 @@ while true; do
     echo "It's higher than that, guess again:"
   else
     echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    UPDATE=$($PSQL "UPDATE users SET games_played = games_played + 1 WHERE name='$USER'")
     break
   fi
 done
